ef8a19a42b48c70f9d3cab52377dbd83db6d319f
Niko Häppölä <niko.happola@gmail.com>
2020-02-07 15:52:28 +0200

    tehtävä 3.5

--- index.js
+++ index.js
@@ -46,6 +46,30 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+const generateId = () => {
+  return Math.ceil(Math.random() * 1000)
+}
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (!body.name) {
+    return res.status(400).json({
+      error: 'name missing'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
